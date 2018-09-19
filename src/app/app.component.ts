7fcad23d22b9628d50a77521902f2e89252ac57a
DmitriyIvanko <ivanko.dmitriy@gmail.com>
2018-09-19 16:04:56 +0300

    + fix styles;

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { MockServerResultsService } from "./mock-server-results-service";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   page = new Page();
