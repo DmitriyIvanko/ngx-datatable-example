7b2ed1b4376763f3675a2fa621b4bc994d1bb8c3
DmitriyIvanko <ivanko.dmitriy@gmail.com>
2018-09-19 15:59:19 +0300

    + add basic implementation;

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Page } from "./model/page";
+import { MockServerResultsService } from "./mock-server-results-service";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ngx-datatable';
+  page = new Page();
+  rows = [];
+  cache: any = {};
+
+  constructor(private serverResultsService: MockServerResultsService) {
+    this.page.pageNumber = 0;
+  }
+
+  setPage(pageInfo) {
+    this.page.pageNumber = pageInfo.offset;
+    this.page.size = pageInfo.pageSize;
+
+    // cache results
+    // if(this.cache[this.page.pageNumber]) return;
+    this.serverResultsService.getResults(this.page).subscribe(pagedData => {
+      this.page = pagedData.page;
+      // 
+      // // calc start
+      // const start = this.page.pageNumber * this.page.size;
+      // 
+      // // copy rows
+      // const rows = [...this.rows];
+      // 
+      // // insert rows into new position
+      // rows.splice(start, 0, ...pagedData.data);
+    
+      // set rows to our new rows
+      this.rows = pagedData.data;
+    
+      // add flag for results
+      // this.cache[this.page.pageNumber] = true;
+    });
+  }
+
 }
